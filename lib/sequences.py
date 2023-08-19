dce0f28633e10b22c49aef7080a04c61b3bd74a4
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-08-20 00:54:24 +0300

    complete assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0,1] #initiallizing the empty arr
+
+    for num in range(2, length): #for each num in the range starting from 2 and max being inputted len
+        print(num)
+        next_num = fibonacci_sequence[-1] + fibonacci_sequence[-2] #adds the last num to the second-last num in the seq to get the next
+        fibonacci_sequence.append(next_num)
+
+    return(fibonacci_sequence)
+print(print_fibonacci(10))
